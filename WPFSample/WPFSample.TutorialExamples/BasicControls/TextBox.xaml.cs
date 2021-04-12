1e7e50e6944b65498265fc8eb52ffa6a184e700a
Jose Clodoalves <jrjcsj@gmail.com>
2021-04-11 21:15:47 -0300

    Adição de exemplos de uso do controle Textbox

---
+++ WPFSample/WPFSample.TutorialExamples/BasicControls/TextBox.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFSample.TutorialExamples.BasicControls
+{
+    /// <summary>
+    /// Interaction logic for TextBox.xaml
+    /// </summary>
+    public partial class TextBox : Window
+    {
+        public TextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {         
+            txtStatus.Text   = $"Selection starts at character #{txtContent.SelectionStart} {Environment.NewLine}";
+            txtStatus.Text += $"Selection is {txtContent.SelectionLength} characters(s) long {Environment.NewLine}";
+            txtStatus.Text += $"Selected text: {txtContent.SelectedText}";
+        }
+    }
+}
