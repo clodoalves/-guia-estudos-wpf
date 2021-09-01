3812972571402c5d440bdad25767deaed5e96b4c
Jose Clodoalves <jrjcsj@gmail.com>
2021-08-31 23:32:52 -0300

    Criacao de nova pagina com modos de binding

---
+++ WPFSample/WPFSample.TutorialExamples/Binding/BindingModes.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFSample.TutorialExamples.Binding
+{
+    /// <summary>
+    /// Interaction logic for BindingModes.xaml
+    /// </summary>
+    public partial class BindingModes : Window
+    {
+        public BindingModes()
+        {
+            InitializeComponent();
+        }
+    }
+}
