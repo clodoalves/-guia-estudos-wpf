e76b4952994833e28c874da2252006aa72b5a7e1
Jose Clodoalves <jrjcsj@gmail.com>
2022-01-02 17:44:15 -0300

    Adição de exemplo de StackPanel control

---
+++ WPFSample/WPFSample.TutorialExamples/Panels/StackPanelControl/HorizontalOrientation.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFSample.TutorialExamples.Panels.StackPanelControl
+{
+    /// <summary>
+    /// Interaction logic for HorizontalOrientation.xaml
+    /// </summary>
+    public partial class HorizontalOrientation : Window
+    {
+        public HorizontalOrientation()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDefault_Click(object sender, RoutedEventArgs e)
+        {
+            FirstButton.VerticalAlignment = VerticalAlignment.Stretch;
+            SecondButton.VerticalAlignment = VerticalAlignment.Stretch;
+            ThirdButton.VerticalAlignment = VerticalAlignment.Stretch;
+            FourthButton.VerticalAlignment = VerticalAlignment.Stretch;
+            FifthButton.VerticalAlignment = VerticalAlignment.Stretch;
+            SixthButton.VerticalAlignment = VerticalAlignment.Stretch;
+        }
+
+        private void BtnAlignment_Click(object sender, RoutedEventArgs e)
+        {
+            FirstButton.VerticalAlignment = VerticalAlignment.Top;
+            SecondButton.VerticalAlignment = VerticalAlignment.Center;
+            ThirdButton.VerticalAlignment = VerticalAlignment.Bottom;
+            FourthButton.VerticalAlignment = VerticalAlignment.Bottom;
+            FifthButton.VerticalAlignment = VerticalAlignment.Center;
+            SixthButton.VerticalAlignment = VerticalAlignment.Top;
+        }
+    }
+}
---
+++ WPFSample/WPFSample.TutorialExamples/Panels/StackPanelControl/VerticalOrientation.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFSample.TutorialExamples.Panels.StackPanelControl
+{
+    /// <summary>
+    /// Interaction logic for VerticalOrientation.xaml
+    /// </summary>
+    public partial class VerticalOrientation : Window
+    {
+        public VerticalOrientation()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDefault_Click(object sender, RoutedEventArgs e)
+        {
+            FirstButton.HorizontalAlignment = HorizontalAlignment.Stretch;
+            SecondButton.HorizontalAlignment = HorizontalAlignment.Stretch;
+            ThirdButton.HorizontalAlignment = HorizontalAlignment.Stretch;
+            FourthButton.HorizontalAlignment = HorizontalAlignment.Stretch;
+            FifthButton.HorizontalAlignment = HorizontalAlignment.Stretch;
+            SixthButton.HorizontalAlignment = HorizontalAlignment.Stretch;
+        }
+
+        private void BtnAlignment_Click(object sender, RoutedEventArgs e)
+        {
+            FirstButton.HorizontalAlignment = HorizontalAlignment.Left;
+            SecondButton.HorizontalAlignment = HorizontalAlignment.Center;
+            ThirdButton.HorizontalAlignment = HorizontalAlignment.Right;
+            FourthButton.HorizontalAlignment = HorizontalAlignment.Right;
+            FifthButton.HorizontalAlignment = HorizontalAlignment.Center;
+            SixthButton.HorizontalAlignment = HorizontalAlignment.Left;
+        }
+    }
+}
